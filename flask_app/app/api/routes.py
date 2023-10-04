a3b0d84e843220c2d43d3bf982ce73c8be745bd7
mattiaqa <888515@stud.unive.it>
2023-10-04 12:13:21 +0200

    added first api route

--- flask_app/app/api/routes.py
+++ flask_app/app/api/routes.py
@@ -7,12 +7,13 @@ from app.models.exam import Exam
 from app.models.student import Student
 from app.models.test import Test
 
+
 @bp.route('/getAppelli', methods=['GET'])
-#@jwt_required()
+@jwt_required()
 def getAppelli():
-   # current_user = get_jwt_identity()
+   #current_user = get_jwt_identity()
     
-    #result = session.query(Test).join(Exam, Test.ExamId==Exam.IdExam).join(Student, Exam.StudentBadgeNumber == Student.BagdeNumber).filter_by(Student.BadgeNumber == current_user)
-    result = session.query(Test).all()
+   #result = session.query(Test).join(Exam, Test.ExamId==Exam.IdExam).join(Student, Exam.StudentBadgeNumber == Student.BagdeNumber).filter_by(Student.BadgeNumber == current_user)
+   result = session.query(Test).all()
     
-    return jsonify()
+   return jsonify()
