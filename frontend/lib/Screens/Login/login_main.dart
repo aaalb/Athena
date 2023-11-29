d040d347f8e6cf1f752f7a54e0ea5d9a9c233c9d
aaalb <albert1alibe@gmail.com>
2023-11-29 15:36:56 +0100

    Login quasi finito

--- frontend/lib/Screens/Login/login_main.dart
+++ frontend/lib/Screens/Login/login_main.dart
@@ -33,11 +33,30 @@ Future<bool> login(String email, String password) async {
   return false;
 }
 
-class LoginPage extends StatelessWidget {
-  LoginPage({super.key});
+class LoginPage extends StatefulWidget
+{
+  @override
+  State<LoginPage> createState() => LoginPageState();
+}
 
+class LoginPageState extends State<LoginPage> 
+{
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+
+  bool loginFailed = false;
+
+  Color borderColor = Color.fromARGB(0, 214, 25, 25); 
+  String errorText = "";
+  List<double> elevations = [2, 2];
+
+  FocusNode FOCemail_in = FocusNode(debugLabel: "email");
+  FocusNode FOCemail_out = FocusNode(debugLabel: "email focus", skipTraversal: true);
+  FocusNode FOCpass_in = FocusNode(debugLabel: "pass");
+  FocusNode FOCpass_out = FocusNode(debugLabel: "pass focus", skipTraversal: true);
+  FocusNode FOCbutton_in = FocusNode(debugLabel: "bottone");
+  FocusNode FOCbutton_out = FocusNode(debugLabel: "bottone", skipTraversal: true);
+
 
   @override
   Widget build(BuildContext context) 
@@ -61,154 +80,229 @@ class LoginPage extends StatelessWidget {
             ),
           ),
 
-          Padding
+          SingleChildScrollView
           (
-            padding: EdgeInsets.all(20),
-            child : Container
+            child: Padding
             (
-              //height: 500,
-              width: 390,
-              decoration: BoxDecoration
+              padding: EdgeInsets.all(20),
+              child : Container
               (
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
-                boxShadow: const
-                [
-                  BoxShadow
-                  (
-                    offset: Offset(0.0, 3.0),
-                    blurRadius: 20,
-                  ),
-                ],
-              ),
-              child: Padding
-              (
-                padding: const EdgeInsets.fromLTRB(55, 80, 55, 30),
-                child: Column
+                width: 390,
+                decoration: BoxDecoration
                 (
-                  children: 
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: const
                   [
-                    const Text
+                    BoxShadow
                     (
-                      "Benvenuto",
-                      style: TextStyle
-                      (
-                        fontFamily: "SourceSansPro",
-                        fontSize: 30,
-                        fontWeight: FontWeight.w700,
-                      ),
+                      offset: Offset(0.0, 3.0),
+                      blurRadius: 20,
                     ),
-                    
-                    Padding
-                    (
-                      padding: const EdgeInsets.only(top: 37),
-                      child: Form
-                      (
-                        child: Column
-                        (
-                          children: 
-                          [
-                            Material
-                            (
-                              elevation: 10,
-                              borderRadius: BorderRadius.circular(10.0),
-                              child: TextField
-                              (
-                                controller: emailController,
-                                keyboardType: TextInputType.emailAddress,
-                                textInputAction: TextInputAction.next,
-                                decoration: const InputDecoration
-                                (
-                                  hintText: "Email",
-                                ),
-                              )
-                            ),
-
-                            Material
-                            (
-                              elevation: 10,
-                              borderRadius: BorderRadius.circular(10.0),
-                              child: TextField
-                              (
-                                controller: passwordController,
-                                keyboardType: TextInputType.emailAddress,
-                                textInputAction: TextInputAction.next,
-                                obscureText: true,
-                                decoration: const InputDecoration
-                                (
-                                  hintText: "Password",
-                                ),
-                              )
-                            ),
-
-                            ElevatedButton
-                            (
-                              style: ButtonStyle
-                              (
-                                overlayColor: const MaterialStatePropertyAll<Color>(Color.fromARGB(255, 53, 33, 10)),
-                                shape: MaterialStateProperty.all<RoundedRectangleBorder>(RoundedRectangleBorder(borderRadius: BorderRadius.circular(25.0))),
-                                fixedSize: MaterialStateProperty.all<Size>(Size(120, 50)),
-                              ),
-                              onPressed: ()
-                              {
-                                login(emailController.text.toString(), passwordController.text.toString())
-                                  .then((value) => (value) ? context.go('/libretto') : {});
-                              },
-                              child: const Padding
-                              (
-                                padding: EdgeInsets.symmetric(), 
-                                child: Text
-                                (
-                                  "ENTRA",
-                                  style: TextStyle
-                                  (
-                                    fontFamily: "SourceSansPro",
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.w600,
-                                  ),
-                                ),
-                              )
-                            ),
-                          ].map((widget) => Padding(
-                            padding: const EdgeInsets.only(bottom: 20),
-                            child: widget,
-                          )).toList(),
-                        )
-                      )
-                    ),
-                    SvgPicture.asset('images/cf-logo.svg'),
                   ],
                 ),
+                child: Padding
+                (
+                  padding: const EdgeInsets.fromLTRB(55, 80, 55, 30),
+                  child: Column
+                  (
+                    children: 
+                    [
+                      Image.asset("images/cf-logo.png", fit: BoxFit.fill,),
 
-              ) 
-            )
+                      /*const Text
+                      (
+                        "Benvenuto",
+                        style: TextStyle
+                        (
+                          fontFamily: "SourceSansPro",
+                          fontSize: 30,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),*/
+                      
+                      Padding
+                      (
+                        padding: const EdgeInsets.only(top: 37),
+                        child: Form
+                        (
+                          child: Column
+                          (
+                            children: 
+                            [
+                              Text(errorText, style: const TextStyle
+                              (
+                                color: Colors.red
+                              ),),
+                              Material
+                              (
+                                elevation: elevations[0],
+                                borderRadius: BorderRadius.circular(10.0),
+                                child: Focus
+                                (
+                                  focusNode: FOCemail_out,
+                                  child: TextField
+                                  (
+                                    focusNode: FOCemail_in,
+                                    controller: emailController,
+                                    keyboardType: TextInputType.emailAddress,
+                                    textInputAction: TextInputAction.next,
+                                    decoration: InputDecoration
+                                    (
+                                      hintText: "Email",
+                                      focusedBorder: OutlineInputBorder
+                                      (
+                                        borderRadius: BorderRadius.circular(10.0),
+                                        borderSide: BorderSide(color: borderColor),
+                                      ),
+                                      enabledBorder: OutlineInputBorder
+                                      (
+                                        borderRadius: BorderRadius.circular(10.0),
+                                        borderSide: BorderSide(color: borderColor),
+                                      ),
+                                    ),
+                                  ),
+                                  onFocusChange: (hasFocus) => _onFocusChange(hasFocus: hasFocus, callerId: 0)
+                                )
+                              ),
+
+                              Material
+                              (
+                                elevation: elevations[1],
+                                borderRadius: BorderRadius.circular(10.0),
+                                child: Focus
+                                (
+                                  focusNode: FOCpass_out,
+                                  child: TextField
+                                  (
+                                    controller: passwordController,
+                                    keyboardType: TextInputType.emailAddress,
+                                    textInputAction: TextInputAction.next,
+                                    obscureText: true,
+                                    decoration: InputDecoration
+                                    (
+                                      hintText: "Password",
+                                      focusedBorder: OutlineInputBorder
+                                      (
+                                        borderRadius: BorderRadius.circular(10.0),
+                                        borderSide: BorderSide(color: borderColor),
+                                      ),
+                                      enabledBorder: OutlineInputBorder
+                                      (
+                                        borderRadius: BorderRadius.circular(10.0),
+                                        borderSide: BorderSide(color: borderColor),
+                                      )
+                                    ),
+                                    focusNode: FOCpass_in,
+                                  ),
+                                  onFocusChange: (hasFocus) => _onFocusChange(hasFocus: hasFocus, callerId: 1),
+                                )
+                              ),
+
+                              Focus(
+                                focusNode: FOCbutton_out,
+                                child: ElevatedButton
+                                (
+                                  focusNode: FOCbutton_in,
+                                  style: ButtonStyle
+                                  (
+                                    overlayColor: const MaterialStatePropertyAll<Color>(Color.fromARGB(255, 53, 33, 10)),
+                                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(RoundedRectangleBorder(borderRadius: BorderRadius.circular(25.0))),
+                                    fixedSize: MaterialStateProperty.all<Size>(Size(120, 50)),
+                                  ),
+                                  onPressed: ()
+                                  {
+                                    login(emailController.text.toString(), passwordController.text.toString())
+                                      .then((value) => (value) ? context.go('/libretto') : 
+                                      {
+                                        setState(() {
+                                          borderColor = Color.fromARGB(255, 255, 0, 0);
+                                          errorText = "Email o password errati";
+                                          loginFailed = true;
+                                          
+                                          passwordController.clear();
+                                        })
+                                      });
+                                  },
+                                  child: const Padding
+                                  (
+                                    padding: EdgeInsets.symmetric(), 
+                                    child: Text
+                                    (
+                                      "ENTRA",
+                                      style: TextStyle
+                                      (
+                                        fontFamily: "SourceSansPro",
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.w600,
+                                      ),
+                                    ),
+                                  )
+                                ),
+                                onFocusChange: (hasFocus) => _onFocusChange(hasFocus: hasFocus, callerId: -1),
+                              ),
+
+                            ].map((widget) => Padding(
+                              padding: const EdgeInsets.only(bottom: 20),
+                              child: widget,
+                            )).toList(),
+                          )
+                        )
+                      ),
+                    ],
+                  ),
+
+                ) 
+              )
+            ),
           )
         ]
       ) 
     );
-    /*
-    return const Background(
-      child: SingleChildScrollView(
-        child: Row(
-          children: [
-            Expanded(
-              child: LoginScreenTopImage(),
-            ),
-            Expanded(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    width: 450,
-                    child: LoginForm(),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-    */
+  
+  }
+
+
+  void ResetFormState()
+  {
+    if(loginFailed)
+    {
+      setState(() 
+      {
+        borderColor = Color.fromARGB(0, 255, 0, 0);
+        errorText = "";
+        loginFailed = false;
+      });
+    }
+  }
+
+  void Elevate({required int index})
+  {
+    setState(() {
+      for(int i = 0; i < elevations.length; i++)
+      {
+        elevations[i] = (i == index) ? 10 : 2;
+      }
+    });
+  }
+
+  void LowerAll()
+  {
+    Elevate(index: -1);
+  }
+
+  void _onFocusChange({required bool hasFocus, required int callerId})
+  {
+    if(hasFocus)
+    {
+      ResetFormState();
+      Elevate(index: callerId);
+    }
+  }
+
+  void onPressed()
+  {
+    debugPrint("Debug: ${FocusManager.instance.primaryFocus?.debugLabel}");
   }
 }
+
