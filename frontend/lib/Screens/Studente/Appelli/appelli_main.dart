2f99dc4f19c2e18f27792d1026cbbfad2a281fc9
mattiaqa <888515@stud.unive.it>
2023-12-07 17:36:26 +0100

    esami docente

--- frontend/lib/Screens/Studente/Appelli/appelli_main.dart
+++ frontend/lib/Screens/Studente/Appelli/appelli_main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'package:frontend/Screens/Studente/side_menu.dart';
 import './appelli_screen.dart';
 
 class Appelli extends StatefulWidget {
@@ -14,16 +13,16 @@ class _LibrettoState extends State<Appelli> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      drawer: SideMenu(),
+      //drawer: SideMenu(),
       body: SafeArea(
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Expanded(
+            /*Expanded(
               // default flex = 1
               // and it takes 1/6 part of the screen
               child: SideMenu(),
-            ),
+            ),*/
             Expanded(
               // It takes 5/6 part of the screen
               flex: 5,
